5989062865d9f2220275b52308eada1e2179cbfa
Juan <juanmolina1208@outlook.es>
2024-01-20 16:53:50 -0500

    Adecuaciones realizadas

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
-import crypto from 'crypto'
 class ProductManager {
     constructor() {
         this.products = [];
-    }
-    
-    addProduct(productName, description, price, thumbnail, code, stock) {
+        this.cont = 0;
+    };
+
+    addProduct(productName, description, price, thumbnail, code,stock) {
+        if (!productName || !description || !price || !thumbnail ||  !stock) {
+            console.log("Todos los campos son obligatorios.");
+            return;
+        };
         let exists = this.products.some(product => product.code === code)
-        
-        if (!exists&& (!productName ||  !description  ||  !price || !thumbnail || !stock))  {
-            console.log("Todos lo campos son obligatorios.");
-        } else  if (exists){
-            console.log("El código ya existe. No se puede agregar el producto, pero los demás campos son válidos.");
-        } else {
-            /*productName.id = crypto.randomBytes(10).toString('hex')*/
-            let id = crypto.randomBytes(5).toString('hex')
-            let newProduct = {
-                title: productName,
-                description: description,
-                price : price,
-                thumbnail : "ruta de imagen",
-                code: code,
-                stock: stock,
-                id : id
+        let nameExists = this.products.some(product => product.title === productName);
+        if (exists || nameExists) {
+            console.log("El nombre o el codigo ya existe. No se puede agregar el producto.");
+            return;
+        }else {
+        this.cont++;
+        let newProduct = {
+            title: productName,
+            description: description,
+            price: price,
+            thumbnail: thumbnail, 
+            code: this.cont,
+            stock: stock,
             };
 
-            this.products.push(newProduct);
-            console.log("Producto agregado correctamente.");
+        this.products.push(newProduct);
+        console.log("Producto agregado correctamente.");
         }
-    }
+
+    } 
     getProducts() {
         return this.products;
-    }
-    getProductById (productId){
-        let product = this.products.find(product => product.id === productId);
+    };
 
-        if (product) {
-            return product;
-        } else {
-            console.log("ERROR : Not found");
-        }
-    }
-};
-
+    getProductById(productId) {
+        let product = this.products.find(product => product.code === productId);
+        return product ? product : "ERROR: Not found";
+    };
+}
 
